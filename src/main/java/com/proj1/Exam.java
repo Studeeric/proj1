2c979def0b4c93326d79afec70d77918ae4a69a9
schooljosep65 <21146632@student.hhs.nl>
2022-03-10 15:04:41 +0100

    Exam aangemaakt

---
+++ src/main/java/com/proj1/Exam.java
@@ -0,0 +1,51 @@
+package com.proj1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exam {
+    private String name;
+    private String category;
+    public static ArrayList<Exam> examList;
+    private ArrayList<Question> questionList;
+    private Scanner scanner = new Scanner(System.in);
+
+
+    /*  ATTENTION
+        Exam kan pas goed getest worden zodra Exam en Question en Student gemerged zijn.
+        Questions moeten nog toegevoegd worden bij initialization.
+    */
+
+    public Exam(String name, String category){
+        this.name = name;
+        this.category = category;
+        questionList.add(null);
+        examList.add(this);
+    }
+
+    public void startExam(Student student){
+        int correct = 0;
+        for (int i = 0; i < questionList.length; i++) {
+            System.out.println("Vraag " + i + ":");
+            questionList.get(i).askQuestion();
+            if(questionList.get(i).checkAnswer(scanner.nextLine())){
+                correct++;
+            }
+        }
+        checkPassed(correct, student);
+    }
+
+    public void checkPassed(int correct, Student student){
+        if(correct >= (questionList.length/2)){
+            student.behaaldeExamens.add(this);
+            System.out.println("Gefeliciteerd! Je hebt het examen gehaald.\n" +
+                                "Je hebt " + correct + " van de " + questionList.length + "vragen goed.");
+        } else {
+            System.out.println("Helaas... Je hebt het examen niet gehaald.\n" +
+                                "Je hebt " + correct + " van de " + questionList.length + "vragen goed.\n" +
+                                "Volgende keer beter!");
+        }
+
+    }
+
+}
