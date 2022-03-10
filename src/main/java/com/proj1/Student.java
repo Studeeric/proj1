738a4408f4745014fde7998a9d0c427601bbf1ec
WesselMH <w.m.horsthuis@live.nl>
2022-03-10 15:03:49 +0100

    meer getters

--- src/main/java/com/proj1/Student.java
+++ src/main/java/com/proj1/Student.java
@@ -5,13 +5,20 @@ import java.util.ArrayList;
 public class Student {
     private String name;
     private int studentNumber;
-    private static ArrayList<Student> studentList;
-    //private Exam examen;
+    public static ArrayList<Student> studentList;
+    private Exam examen;
 
     public Student(String name, int studentNumber){
         this.name = name;
         this.studentNumber = studentNumber;
 
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public int getStudentNumber(){
+        return this.studentNumber;
     }
 
     public void getPassed(){
