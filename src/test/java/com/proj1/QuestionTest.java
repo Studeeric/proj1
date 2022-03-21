5c9730288ec3bf8e61ff19a78c2d9e7f0369a899
WesselMH <w.m.horsthuis@live.nl>
2022-03-21 12:00:05 +0100

    tests question

--- src/test/java/com/proj1/QuestionTest.java
+++ src/test/java/com/proj1/QuestionTest.java
@@ -1,9 +1,36 @@
 package com.proj1;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
 
 public class QuestionTest {
-    
+
+    Question testQuestion = new Question("Wie werkt aan deze test?", "Wessel");
+
+
+    //testen of de vraag over een komt
+    @Test
+    public void askQuestionTest(){
+        String TestText = "Wie werkt aan deze test?";
+        
+        String resultText = testQuestion.askQuestion();
+
+        assertEquals(resultText, TestText);
+             
+    }
+
+    //test of het antwoord klopt
+    @Test
+    public void checkAnswerTest(){
+        String TestAnswer = "Wessel";
+        String FoutTestAnswer = "Burton";
+
+        assertTrue(testQuestion.checkAnswer(TestAnswer));
+        assertFalse(testQuestion.checkAnswer(FoutTestAnswer));
+        
+    }
 }
+
