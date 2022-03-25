17e76e009ec8c279da8cd46e99a2f413b71afaa7
WesselMH <w.m.horsthuis@live.nl>
2022-03-25 14:41:59 +0100

    hij heeft gee vinkies

--- src/test/java/com/proj1/ExamTest.java
+++ src/test/java/com/proj1/ExamTest.java
@@ -1,10 +1,35 @@
 package com.proj1;
 
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.Test;
 
 public class ExamTest {
+
+    Exam TestExam = new Exam("OPT1", "HBO-ICT");
+
+    //test of de naam wordt opgevraagd
+    @Test
+    public void getNameTest(){
+        String TestName = "OPT1";
+        String foutTestName = "Biologie";
+        
+        String resultName = TestExam.getName();
+        assertEquals(TestName, resultName);
+        assertNotEquals(foutTestName, resultName);
+    }
+
+    //test of de category wordt opgevraagd
+    @Test
+    public void getCategoryTest(){
+        String TestCategory = "HBO-ICT";
+        String foutTestCategory = "Beta";
+
+        String resultCategory = TestExam.getCategory();
+        assertEquals(TestCategory, resultCategory);
+        assertNotEquals(foutTestCategory, resultCategory);
+    }
     
 }
