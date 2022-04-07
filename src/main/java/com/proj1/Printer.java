a9bc3142ea6124d2a66d300f18367a06b570ab04
Studeeric <21146632@student.hhs.nl>
2022-04-07 16:42:59 +0200

    printer fixed (?)

--- src/main/java/com/proj1/Printer.java
+++ src/main/java/com/proj1/Printer.java
@@ -1,26 +1,15 @@
 package com.proj1;
 
-public class Printer implements IPrinter {
+public class Printer implements IPrinter{
 
     @Override
     public void print(String in) {
-        System.out.println(in);
-        
+        System.out.println(in);        
     }
- 
-    
-}   
-
-abstract class FakePrinter implements IPrinter{
-    public String lastoutput;
-    @Override
-    public void print(String in) {
-        this.lastoutput = in;
-    }
-
 }
 
-interface IPrinter{
-    public void print(String in);
 
+
+interface IPrinter{
+    void print(String in);
 }
