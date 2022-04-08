59c329ebf0877be31c75c8ed9cc37602b46eeead
Studeeric <21146632@student.hhs.nlm>
2022-04-08 14:29:58 +0200

    verdere changes

--- src/main/java/com/logic/IScanner.java
+++ src/main/java/com/logic/IScanner.java
@@ -4,3 +4,20 @@ public interface IScanner {
   int nextInt();
   String nextLine();
 }
+
+class RealScanner implements IScanner {
+
+  public RealScanner() {
+  }
+
+  @Override
+  public int nextInt() {
+    return 0;
+  }
+
+  @Override
+  public String nextLine() {
+    return null;
+  }
+
+}
