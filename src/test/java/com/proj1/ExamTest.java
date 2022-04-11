eea41768747d0dcaa424e8253bfa89baa498e462
Studeeric <21146632@student.hhs.nl>
2022-04-11 14:41:54 +0200

    100% coverage exam

--- src/test/java/com/proj1/ExamTest.java
+++ src/test/java/com/proj1/ExamTest.java
@@ -2,7 +2,10 @@ package com.proj1; import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+
 import com.logic.Exam;
+import com.logic.Question;
 import com.logic.Student;
 
 import org.junit.jupiter.api.Test;
@@ -13,6 +16,10 @@ public class ExamTest {
     static Exam testExam = new Exam("OPT1", "HBO-ICT");
     static Student testStudent = new Student("Pietje", 21146633);
     static Student testStudent2 = new Student("Klaasje", 21146634);
+    static ArrayList<String> questionStringList = new ArrayList<>();
+    static Question question = new Question("prompt", questionStringList, "1");
+    static Question question2 = new Question("prompt", questionStringList, "1");
+
 
     //test of de naam wordt opgevraagd
     @Test
@@ -46,17 +53,38 @@ public class ExamTest {
         assertEquals(testExam, testStudent.behaaldeExamens.get(0));
         assertTrue(testStudent2.behaaldeExamens.isEmpty());
     }
-    
-    @Test
-    public void testRandom(){
-        Exam.printAllExams(testScanner);
-        testScanner.setInt(3);
-        assertEquals(3, testScanner.nextInt());
-    }
 
     @Test
     public void testGetExam(){
         assertEquals(testExam, Exam.getExam(0));
+        Exam.examList.remove(0);
+        Exam.printAllExams(testScanner);
+        assertTrue(Exam.examList.isEmpty());
     }
     
+    @Test
+    public void testAddQuestion(){
+        testExam.addQuestion(question);
+        assertEquals(question, testExam.questionList.get(0));;
+    }
+
+    @Test
+    public void testGetQuestionlist(){
+        assertEquals(testExam.questionList, testExam.getQuestionList());
+    }
+
+    @Test
+    public void testStartExam(){
+        questionStringList.add("random");
+        testExam.addQuestion(question);
+        testExam.addQuestion(question2);
+        testScanner.setInt(1);
+        testScanner.setString("1");
+        testExam.startExam(testStudent, testScanner);
+        testScanner.setInt(2);
+        testScanner.setString("2");
+        testExam.startExam(testStudent2, testScanner);
+        assertEquals(testStudent.behaaldeExamens.get(0), testExam);
+        assertTrue(testStudent2.behaaldeExamens.isEmpty());
+    }
 }
